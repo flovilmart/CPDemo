7b965a34ad40e591e34e2d739b9d15f5ea35087b
Florent Vilmart <florentvilmart@me.com>
2016-10-12 09:42:26 -0400

    Initial Commit

---
+++ CPDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPDemo
+//
+//  Created by Florent Vilmart on 16-10-12.
+//  Copyright © 2016 flovilmart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
